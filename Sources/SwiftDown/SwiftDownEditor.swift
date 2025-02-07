5ce468d84c012318ea61059d49f8c9bd3ec2a82b
thmmhsn <95959772+thmmhsn@users.noreply.github.com>
2025-02-07 21:23:07 +0500

    Update SwiftDownEditor.swift

--- Sources/SwiftDown/SwiftDownEditor.swift
+++ Sources/SwiftDown/SwiftDownEditor.swift
@@ -42,11 +42,22 @@ public struct SwiftDownEditor: UIViewRepresentable {
       self.onSelectionChange = onSelectionChange
     }
 
-    public func makeUIView(context: Context) -> SwiftDown {
+    public func makeUIView(context: Context) -> SwiftDown {.
       let swiftDown = SwiftDown(frame: .zero, theme: theme)
       swiftDown.storage.markdowner = { self.engine.render($0, offset: $1) }
       swiftDown.storage.applyMarkdown = { m in Theme.applyMarkdown(markdown: m, with: self.theme) }
       swiftDown.storage.applyBody = { Theme.applyBody(with: self.theme) }
+      swiftDown.storage.applyBody = { 
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 10.0  // Set your desired line spacing here
+
+        return [
+            .paragraphStyle: paragraphStyle,
+            .font: UIFont.systemFont(ofSize: 14),  // Adjust font as necessary
+            .foregroundColor: UIColor.label
+        ]
+        
+      }
       swiftDown.delegate = context.coordinator
       swiftDown.isEditable = isEditable
       swiftDown.isScrollEnabled = true
